75377c0b16ae73b9a2396b88558ed83bcfc2af36
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-11 22:56:43 +0300

    Create updater.py

---
+++ updater.py
@@ -0,0 +1,8 @@
+###############################################################
+import bpy
+from bpy import data as D
+from bpy import context as C
+from mathutils import *
+from math import *
+
+###############################################################
