5a84e7e9751859ae4b810b4359ec140c01e07ed2
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-11 14:00:10 +0300

    Create edit_in_gui_result_in_python.py

---
+++ edit_in_gui_result_in_python.py
@@ -0,0 +1,6 @@
+# in python console typical results after editing in Viewport in Blender
+bpy.ops.outliner.delete()
+
+bpy.ops.mesh.primitive_cylinder_add(radius=1, depth=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+
+bpy.ops.mesh.primitive_uv_sphere_add(radius=1, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
