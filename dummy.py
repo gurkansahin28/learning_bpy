e467cebd0c391570752a0085b9c768119171f0d8
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-11 13:48:19 +0300

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,7 +1,17 @@
 ################################################################
-# checking the Python release
+# Checking the Python release
 >>> import sys
 >>> sys.version
+###############################################################
+# Checking the Blender release in the Python script
+>>> import bpy
+>>> bpy.app.version
+###############################################################
+# Achieving bpy.app.version data in detail
+>>> major, minor, micro = bpy.app.version
+>>> print("Major version:", major)
+>>> print("Minor version:", minor)
+>>> print("Micro version:", micro)
 ################################################################
 import bpy
 
