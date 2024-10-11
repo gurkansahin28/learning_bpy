b7dc9b3605f0a41775b407109ac1e6824087dd0d
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-11 18:44:26 +0300

    Update edit_in_gui_result_in_python.py

--- edit_in_gui_result_in_python.py
+++ edit_in_gui_result_in_python.py
@@ -1,5 +1,6 @@
 #############################################################################################################################
 # in python console typical results after editing in Viewport in Blender
+# # https://docs.blender.org/api/current/index.html
 bpy.ops.outliner.delete()
 
 bpy.ops.mesh.primitive_cylinder_add(radius=1, depth=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
