c77368374db289c86c5e6dfd69b22a16d782e486
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-11-08 22:33:05 +0300

    Create sphere.py

---
+++ creating_meshes/sphere.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 16:42:40 2024
+
+@author: gurkan
+"""
+
+import bpy
+
+# Create a sphere and name it "MySphere"
+bom = bpy.ops.mesh
+bom.primitive_uv_sphere_add(radius=1)
+sphere = bpy.context.object
+sphere.name = "MySphere"  # Assign a specific name to the sphere
+sphere.location = (0, 0, 0)
+sphere.rotation_euler = (1.0, 0.3, 0.2)
+bpy.data.objects['MySphere'].location = (2, 2, 0)
+
